d3adafd4d4c1f4d950255095443839608c8fc25f
Umair Naveed <unaveed@gmail.com>
2018-07-21 10:25:29 -0600

    Initial commit, added some third party libraries

---
+++ app/src/main/java/com/umair/app/doordash/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.umair.app.doordash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
